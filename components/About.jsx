6cbea462f8cafec3fc5a426a1f43369812a3674a
Ramona <rmn.neagu@yahoo.ro>
2026-02-18 23:19:25 +0200

    Retouches

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <section id="about">
             <h2>About</h2>
-            <p>I am a frontend developer with a particular interest in making things simple and beautiful. I try to keep up with design trends, accesability and best practices in web development. I am always looking for new things to learn.</p>
+            <p>I am a frontend developer with a particular interest in making things simple and beautiful. I try to keep up with design trends, accesibility and best practices in web development. I am always looking for new things to learn.</p>
         </section>
     )
 }
