38c834f36e2e9d711b34040e7031a3fa1aec003c
Ramona <rmn.neagu@yahoo.ro>
2026-04-08 08:22:39 +0300

    Fix base path configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/digital-business-card/"
+  base: '/digital-business-card/',
 })
